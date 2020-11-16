9c7fd7da4d591223b11f753df4d44a6d69b33903
Jean Santos <jean.santos@compasso.com.br>
2020-11-16 00:15:04 -0300

    Update models and database connection

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String uid;
+  final String name;
+  final String email;
+  final String photo;
+  final String phoneNumber;
+  final String typeLogin;
+
+  UserModel(this.uid, this.name, this.email, this.photo, this.phoneNumber,
+      this.typeLogin);
+}
+
+UserModel userModel;
